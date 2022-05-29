993915a06b48e67a7ee927f024f70f6bb95ca4fa
Clark Alvarez <clark.alvarez@canstar.com.au>
2022-05-29 18:55:07 +0800

    added ui changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,144 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>React Posts Sharer</h1>
-      </header>
+    <div className="container">
+      <div className="row row-cols-3">
+        <div className="col">
+          <div className="card border-dark mb-3" style={{ width: "20rem" }}>
+            <div className="card-header">TSLA </div>
+            <div className="card-body">
+              <img
+                className="card-img-top mb-3"
+                src="https://imageio.forbes.com/specials-images/imageserve/5f1f37a40a5db2c8275972c0/-Photo-by-JOHN-THYS-AFP-via-Getty-Images-/960x0.jpg?format=jpg&width=960"
+                alt="Card image cap"
+              />
+
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Price</label>
+                    <label className="text-right">$45</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Change</label>
+                    <label className="text-right">%156</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Market Cap</label>
+                    <label className="text-right">$344,343</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card border-dark mb-3" style={{ width: "20rem" }}>
+            <div className="card-header">TSLA </div>
+            <div className="card-body">
+              <img
+                className="card-img-top mb-3"
+                src="https://imageio.forbes.com/specials-images/imageserve/5f1f37a40a5db2c8275972c0/-Photo-by-JOHN-THYS-AFP-via-Getty-Images-/960x0.jpg?format=jpg&width=960"
+                alt="Card image cap"
+              />
+
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Price</label>
+                    <label className="text-right">$45</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Change</label>
+                    <label className="text-right">%156</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Market Cap</label>
+                    <label className="text-right">$344,343</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card border-dark mb-3" style={{ width: "20rem" }}>
+            <div className="card-header">TSLA </div>
+            <div className="card-body">
+              <img
+                className="card-img-top mb-3"
+                src="https://imageio.forbes.com/specials-images/imageserve/5f1f37a40a5db2c8275972c0/-Photo-by-JOHN-THYS-AFP-via-Getty-Images-/960x0.jpg?format=jpg&width=960"
+                alt="Card image cap"
+              />
+
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Price</label>
+                    <label className="text-right">$45</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Change</label>
+                    <label className="text-right">%156</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Market Cap</label>
+                    <label className="text-right">$344,343</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card border-dark mb-5" style={{ width: "20rem" }}>
+            <div className="card-header">TSLA </div>
+            <div className="card-body">
+              <img
+                className="card-img-top mb-3"
+                src="https://imageio.forbes.com/specials-images/imageserve/5f1f37a40a5db2c8275972c0/-Photo-by-JOHN-THYS-AFP-via-Getty-Images-/960x0.jpg?format=jpg&width=960"
+                alt="Card image cap"
+              />
+
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Price</label>
+                    <label className="text-right">$45</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Change</label>
+                    <label className="text-right">%156</label>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="column info">
+                    <label className="text-left">Market Cap</label>
+                    <label className="text-right">$344,343</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
